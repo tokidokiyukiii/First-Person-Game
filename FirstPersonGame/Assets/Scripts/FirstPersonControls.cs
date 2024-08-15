20ff6409814993bc68286c66c7c757c68a9bf51d
NAYUVKA <nayuvka.nathan@gmail.com>
2024-08-15 14:55:36 +0200

    change shader for building proj
    commented shader out in first person controls script

--- FirstPersonGame/Assets/Scripts/FirstPersonControls.cs
+++ FirstPersonGame/Assets/Scripts/FirstPersonControls.cs
@@ -1,7 +1,7 @@
 
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.ShaderGraph;
+//using UnityEditor.ShaderGraph;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
