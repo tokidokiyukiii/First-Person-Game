e860fb823e788bdd5ea384e09efd6976207d01fc
tokidokiyukiii <2658083@students.wits.ac.za>
2024-10-11 19:07:16 +0200

    Attempt of counting thoughts

---
+++ FirstPersonGame/Assets/Scripts/ThoughtCount.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ThoughtCount : MonoBehaviour
+{
+    private int thoughtCount = 0;
+
+    public GameObject countPanel;
+    public TMP_Text thoughtCountText;
+
+    public GameObject messagePanel; 
+    public TMP_Text messageText; 
+
+    public void AddThought(int value)
+    {
+        thoughtCount += value;
+
+        if (thoughtCount >= 5)
+        {
+            ShowMessage("Go find the key on the table!");
+        }
+
+        UpdateUI(); 
+    }
+
+    private void ShowMessage(string message)
+    {
+        messagePanel.SetActive(true);
+        countPanel.SetActive(false);
+        messageText.text = message; 
+
+        // StartCoroutine(HideMessageAfterDelay(2f));
+    }
+
+    private void UpdateUI()
+    {
+        if (thoughtCountText != null)
+        {
+            thoughtCountText.text = "Thoughts: " + thoughtCount + "/ 5";
+        }
+    }
+
+    /*private IEnumerator HideMessageAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        messagePanel.SetActive(false); // Hide the message panel
+    }*/
+}
---
+++ FirstPersonGame/Assets/Scripts/Thoughts.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thoughts : MonoBehaviour
+{
+    public int value = 1; 
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) 
+        {
+            other.GetComponent<ThoughtCount>().AddThought(value);
+            //Destroy(gameObject);
+
+        }
+    }
+}
