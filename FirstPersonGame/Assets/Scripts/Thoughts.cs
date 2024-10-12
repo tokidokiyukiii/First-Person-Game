067d65fbe8d738951caca3964f0274019c8957de
UltraFizzyMel <2344445@students.wits.ac.za>
2024-10-12 12:42:09 +0200

    Reapply "End Game and UI Fixes"
    This reverts commit 8d9afe388596bdab67aca4aac22d82ee433d70c7.

--- FirstPersonGame/Assets/Scripts/Thoughts.cs
+++ FirstPersonGame/Assets/Scripts/Thoughts.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class Thoughts : MonoBehaviour
 {
-    public int value = 1; 
+    /*public int value = 1; 
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player")) 
         {
-            other.GetComponent<ThoughtCount>().AddThought(value);
+            other.GetComponent<ThoughtCount>().AddThought();
             //Destroy(gameObject);
 
         }
-    }
+    }*/
+
+    public string writtenThought;
+    public string myThought;
 }
