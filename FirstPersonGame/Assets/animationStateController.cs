ce50a5e03772b9f8773c4dce14dbd2c0e4f89167
NAYUVKA <nayuvka.nathan@gmail.com>
2024-11-22 10:17:58 +0200

    CHARACTER FINAL
    FINAL FIX

--- FirstPersonGame/Assets/animationStateController.cs
+++ FirstPersonGame/Assets/animationStateController.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class animationStateController : MonoBehaviour
 {
+    Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey("w")) 
+        {
+        animator.SetBool("isWalking", true);
+        }
+
+        if (!Input.GetKey("w")) 
+        { 
+        animator.SetBool("isWalking", false);
+        }
     }
 }
